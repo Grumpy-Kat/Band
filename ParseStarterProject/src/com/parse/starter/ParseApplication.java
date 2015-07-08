dddff8a6c7a1e90407ec172c9f5e6ccec304479d
kai <kaiwinsforever@gmail.com>
2015-07-08 21:54:45 +0530

    First commit

---
+++ ParseStarterProject/src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,36 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Initialize Crash Reporting.
+    ParseCrashReporting.enable(this);
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(this, "RJk6pZKbzFQ87dbSeyhsdIjif2xCIWdDrAsy8oo4", "v6rP4WMfoZjgKKxMhynUMXg9Hz4cyaSOCrzty32d");
+   // Parse.initialize(this);
+
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+    // Optionally enable public read access.
+     defaultACL.setPublicReadAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
---
+++ ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -0,0 +1,48 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+public class ParseStarterProjectActivity extends Activity {
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if(ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+
+
+			Intent intent = new Intent(ParseStarterProjectActivity.this, LoginSignupActivity.class);
+			startActivity(intent);
+			finish();
+
+		} else {
+
+			ParseUser currentUser = ParseUser.getCurrentUser();
+
+			if(currentUser != null) {
+
+				Intent intent = new Intent(ParseStarterProjectActivity.this, Welcome.class);
+				startActivity(intent);
+				finish();
+
+			} else {
+
+				Intent intent = new Intent(ParseStarterProjectActivity.this, LoginSignupActivity.class);
+				startActivity(intent);
+				finish();
+
+			}
+
+		}
+
+
+
+		ParseAnalytics.trackAppOpenedInBackground(getIntent());
+	}
+}
